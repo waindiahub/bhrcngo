2411ae22c8a970ef4a18c02c9b6167f1a022f6e9
waindiahub <waindiahub@gmail.com>
2025-09-28 06:24:21 +0530

    Update server.jslolfgjhg
    fghjghj

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ app.use(helmet({
 
 // CORS configuration - Allow all origins
 app.use(cors({
-    origin: '*',
-    credentials: false,
+    origin: true, // Allow all origins
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
